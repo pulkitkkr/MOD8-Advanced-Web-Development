90a400a3c8ff392181531e913facca0686b3531a
pulkitkkr <pulkitkkr@gmail.com>
2022-03-09 12:26:54 -0500

    Lecture 5 Vue JS Material

---
+++ Week2Lec5/src/components/Hero.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  props:{
+    text: String,
+    description: String,
+    shouldShowButton: Boolean,
+    divHtml: String,
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{text}}</h1>
+    <p>{{description}}</p>
+    <button v-if="shouldShowButton">Click</button>
+    <div v-html="divHtml" />
+  </div>
+</template>
+
+<style>
+  .container {
+    background-color: #c3eef2;
+    padding: 50px;
+    min-height: 400px;
+  }
+</style>
---
+++ Week2Lec5/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<template>
+    <div class="navbar-container">
+      <span>Home</span>
+    </div>
+</template>
+
+<style>
+    .navbar-container {
+      background-color: #000;
+      color: #fff;
+    }
+</style>
