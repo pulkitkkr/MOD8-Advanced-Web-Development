b96b46b384d4adc5175a45358d983f82fd653f0b
pulkitkkr <pulkitkkr@gmail.com>
2022-03-17 10:06:30 -0400

    Add MVC Stubs

---
+++ Week3Lec3/2-client-server-MVC-refactoring/client/src/components/Card.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  props: {
+    description: String,
+    title: String
+  }
+}
+</script>
+
+<template>
+  <div class="card-container">
+    <h3>{{title}}</h3>
+    <p>{{description}}</p>
+  </div>
+
+</template>
+
+<style>
+.card-container{
+  max-width: 300px;
+  display: inline-block;
+  background-color: #fefefe;
+  border: 1px solid #000;
+  padding: 30px;
+}
+</style>
